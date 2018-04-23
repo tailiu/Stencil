d0c59ad5bd7c4ab8d5b659dd0df9b0e96f835d70
tailiu <tl67@nyu.edu>
2018-04-23 16:15:05 +0400

    before pulling

--- twitter/db/schema.rb
+++ twitter/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423104905) do
+ActiveRecord::Schema.define(version: 20180423111634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20180423104905) do
   create_table "tweets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
     t.text "content"
     t.integer "reply_to"
     t.bigint "user_id"
