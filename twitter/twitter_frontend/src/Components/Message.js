33ec9bf7e716540d047f0701ce2a055fdf835400
tailiu <tl67@nyu.edu>
2018-05-15 09:51:41 +0400

    finish sending media in messages

--- twitter/twitter_frontend/src/Components/Message.js
+++ twitter/twitter_frontend/src/Components/Message.js
@@ -1,9 +1,27 @@
 import React, {Component} from "react";
-import { ListItem, ListItemText, } from 'material-ui/List';
 import Moment from 'moment';
 import Avatar from 'material-ui/Avatar';
 import { withCookies, Cookies } from 'react-cookie';
 import { instanceOf } from 'prop-types';
+import Card, { CardContent, CardHeader } from 'material-ui/Card';
+
+var styles = {
+    photo: {
+        height: "auto",
+        width: '90%'
+    },
+    video: {
+        height: "auto",
+        width: '100%'
+    },
+    media_container: {
+        textAlign: "center",
+    },
+    text: {
+        whiteSpace: 'normal',
+        wordWrap: 'break-word'
+    }
+}
 
 class Message extends Component {
 
@@ -18,69 +36,90 @@ class Message extends Component {
 
         this.state = {
             user_id: cookies.get('user_id'),
+            base_url: "http://localhost:3000/"
         };
     }
 
-    getLatestUpdatedDateForMessage = () => {
+    getLatestUpdatedDate = () => {
         return Moment(this.props.message.updated_at).format('MMMM Do, YYYY - h:mm A');
     }
 
     setStyle = () => {
         const message = this.props.message
 
-        var styles = {
-            listContainer: {
-                float       : 'none', 
-                width       : '20vw',
-                marginLeft  : 0,
-                marginRight : 0
-            },
-            listItem: {
-                whiteSpace: 'normal',
-                wordWrap: 'break-word'
-            }
+        styles.cardContainer = {
+            marginTop       : 5,
+            marginBottom    : 20,
+            float           : 'none', 
+            width           : '40%',
+            marginLeft      : 0,
+            marginRight     : 0,
+            borderRadius    : '20px'
         }
 
         if (message.user_id == this.state.user_id) {
-            styles.listContainer.marginLeft = 'auto'
+            styles.cardContainer.marginLeft = 'auto'
         }
 
         return styles
     }
 
-    getHeaderForMessage = () => {
+    getText = () => {
         const message = this.props.message
-
         var text = ''
-
         if (this.props.current_conversation_type == 'group') {
             text += message.name + ': '
         }
-
         text += this.props.message.content
-
         return text
     }
 
-    getMediaForMessage = () => {
-        
+    getMedia = () => {
+        const message = this.props.message
+
+        if (message.message_media.url != null) {
+            if (message.media_type == 'photo') {
+                return (
+                    <CardContent style={styles.media_container}>
+                        <img style={styles.photo} src={this.state.base_url + this.props.message.message_media.url} />
+                    </CardContent>
+                )
+            } else if (message.media_type == 'video') {
+                return (
+                    <CardContent style={styles.media_container}>
+                        <video style={styles.video} controls>
+                            <source src={this.state.base_url + this.props.message.message_media.url} type="video/mp4"/>
+                        </video>
+                    </CardContent>
+                )
+            }
+        }
+    }
+
+    getAvatar = () => {
+        return this.props.message.name.charAt(0).toUpperCase()
     }
 
     render () {
-        const text = this.getHeaderForMessage();
-        const updatedDate = this.getLatestUpdatedDateForMessage();
-
         const styles = this.setStyle()
 
         return (
-            <ListItem style={styles.listContainer}>
-                <Avatar src={require('../Assets/Images/user_icon.png')} />
-                <ListItemText style={styles.listItem}
-                    primary={text}
-                    secondary={updatedDate}
+            <Card style={styles.cardContainer}>
+                <CardHeader
+                    avatar={
+                        <Avatar>
+                            {this.getAvatar()}
+                        </Avatar>
+                    }
+                    subheader={this.getLatestUpdatedDate()}
                 />
-            </ListItem>
-        )   
+                {this.getMedia()}
+                <CardContent style={styles.text}>
+                    {this.getText()}
+                </CardContent>
+                
+            </Card>
+        )
     }
 }
 
