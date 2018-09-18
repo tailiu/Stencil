d553f7d027ca7d9b109b932c2af81e8fe5bf4713
tailiu <tl67@nyu.edu>
2018-09-18 15:30:01 +0400

    get row id

--- stencil/get_queries.py
+++ stencil/get_queries.py
@@ -1,60 +1,15 @@
 import MySQLdb
 import re
-
-def getDBConn():
-    db_conn = MySQLdb.connect(
-        host   = "127.0.0.1",
-        port   = 3307,
-        user   = "root",
-        passwd = "",
-        db     = "stencil_storage",
-    )
-    return db_conn, db_conn.cursor()
+import utils
 
 def findBetweenStrings(originalStr, str1, str2):
     strStart = originalStr.find(str1) + len(str1)
     strEnd = -1
     if str2 != None: strEnd = originalStr.find(str2)
     return originalStr[strStart : strEnd]
-
-def processConditions(conds):
-    conds = conds.split('and')
-
-    condList = []
-    for cond in conds:
-        cond1 = cond.split('=')
-        condList.append(cond1[0].strip())
     
-    return condList
-
-def removeSpace(l):
-    for i in range(len(l)):
-        l[i] = l[i].strip()
-    return l
-    
-def formAttrStr(attrList):
-    attrStr = '('
-    for i in range(len(attrList)):
-        if i == len(attrList) - 1: attrStr += 'app_schemas.column_name = \'' + attrList[i] + '\')'
-        else: attrStr += 'app_schemas.column_name = \'' + attrList[i] + '\' or '
-    return attrStr
-
-def translateAttributesToBaseTables(app_name, table, attrList):
-    attrStr = formAttrStr(attrList)
-        
-    sql = "SELECT app_schemas.column_name, base_table_attributes.table_name, base_table_attributes.column_name\
-            FROM base_table_attributes INNER JOIN physical_mappings INNER JOIN app_schemas INNER JOIN app_tables INNER JOIN apps\
-            on base_table_attributes.PK = physical_mappings.physical_attribute\
-            and app_schemas.PK = physical_mappings.logical_attribute\
-            and app_tables.PK = app_schemas.table_id\
-            and apps.PK = app_tables.app_id \
-            WHERE app_name = '{0}' and app_tables.table_name = '{1}' and {2}".format(app_name, table, attrStr)
-
-    CUR.execute(sql)
-    return CUR.fetchall()
-
 def findSuppTables(app_name, table, suppAttributes):
-    attrStr = formAttrStr(suppAttributes)
+    attrStr = utils.formAttrStr(suppAttributes)
 
     sql = "SELECT app_schemas.column_name, supplementary_tables.supplementary_table\
             FROM supplementary_tables INNER JOIN app_schemas INNER JOIN app_tables INNER JOIN apps\
@@ -74,12 +29,18 @@ def resolveRequest(query, baseAttributes, suppAttributes):
     for attr in baseAttributes: tables.add(attr[1])
     for attr in suppAttributes: tables.add(attr[1])
 
-    fromTables = ''
-    for table in tables: fromTables += ' join ' + table
-    fromTables = fromTables.replace('join', '', 1)
-    fromTables += ' on '
-    for table in tables: fromTables += ' = ' + table + '.row_id '
-    fromTables = fromTables.replace('=', '', 1)
+    if len(tables) == 1:
+        fromTables = tables.pop()
+        oneTable = fromTables
+    else:
+        fromTables = ''
+        for table in tables: 
+            fromTables += ' join ' + table
+            oneTable = table
+        fromTables = fromTables.replace('join', '', 1)
+        fromTables += ' on '
+        for table in tables: fromTables += ' = ' + table + '.row_id '
+        fromTables = fromTables.replace('=', '', 1)
 
     tablesStart = query.find("from") + len("from")
     tablesEnd = query.find("where")
@@ -102,14 +63,14 @@ def findAllAttributes(app_name, table):
         attrList.append(attr[0])
     return attrList
 
-def translateBasicSelectQuery(query):
+def translateBasicSelectQuery(CUR, query):
     query = query.lower()
     
     table = findBetweenStrings(query, 'from', 'where').strip() # Assume there is only one table
 
     if query.find('*') == -1:
         attributes = findBetweenStrings(query, 'select', 'from').split(',')
-        attributes = removeSpace(attributes)
+        attributes = utils.removeSpace(attributes)
     else: 
         attributes = findAllAttributes('hacker news', table)
         attrStr = ''
@@ -118,12 +79,12 @@ def translateBasicSelectQuery(query):
         query = query.replace('*', attrStr)
         query = query.lower()
 
-    condList = processConditions(findBetweenStrings(query, 'where', None))
-    condList = removeSpace(condList)
+    condList = utils.processConditions(findBetweenStrings(query, 'where', None))
+    condList = utils.removeSpace(condList)
 
     attrList = list(set(attributes).union(condList))
 
-    baseAttributes = translateAttributesToBaseTables('hacker news', table, attrList)
+    baseAttributes = utils.translateAttributesToBaseTables(CUR, 'hacker news', table, attrList)
 
     suppAttributeList = []
     for attr in attrList:
@@ -134,13 +95,15 @@ def translateBasicSelectQuery(query):
                 break
         if not find: suppAttributeList.append(attr)
 
-    suppAttributes = findSuppTables('hacker news', table, suppAttributeList)
+    suppAttributes = ()
+    if len(suppAttributeList) != 0: suppAttributes = findSuppTables('hacker news', table, suppAttributeList)
 
     return resolveRequest(query, baseAttributes, suppAttributes)
-     
+
+
 if __name__ == "__main__":
 
-    CONN, CUR = getDBConn()
+    CONN, CUR = utils.getDBConn()
 
     sql = "SELECT By, Descendents, Id, Retrieved_on, Score\
            FROM story  \
@@ -154,7 +117,7 @@ if __name__ == "__main__":
             FROM comment  \
             WHERE Id = 13075839 + 1 "
 
-    translatedQuery = translateBasicSelectQuery(sql2)
+    translatedQuery = translateBasicSelectQuery(CUR, sql1)
     print translatedQuery
 
     CUR.execute(translatedQuery)
