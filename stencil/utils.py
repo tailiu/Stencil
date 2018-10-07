c2a0f8fad2d8d1945047625904d8b6d219c6bac1
tailiu <tl67@nyu.edu>
2018-10-07 09:58:21 +0400

    trying coakroach

--- stencil/utils.py
+++ stencil/utils.py
@@ -5,7 +5,7 @@ def getDBConn():
     db_conn = MySQLdb.connect(
         host   = "10.224.45.162",
         port   = 3306,
-        user   = "freedom_tai",
+        user   = "tai",
         passwd = "123",
         db     = "stencil_storage",
     )
