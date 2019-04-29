8929401aa2cc8824c830c8daf3a5849bdf4b298d
Zain <zaintq@gmail.com>
2019-04-29 14:32:45 +0400

    adaptive process for physical stencil schema + sqls

--- adaptive_schema/adaptive_schema.py
+++ adaptive_schema/adaptive_schema.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 def getDBConn(db, cursor_dict=False):
     conn = psycopg2.connect(dbname=db, user="cow", password="123456", host="10.230.12.75", port="5432")
-    # conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) 
     if cursor_dict is True:
         cursor = conn.cursor(cursor_factory = RealDictCursor)
     else:
