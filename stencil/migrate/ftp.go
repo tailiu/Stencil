4fc7f2385d28ed5162169096c2282675c6c2785f
Zain <zaintq@gmail.com>
2019-10-10 15:24:45 +0400

    transfer media in migrate package

--- stencil/migrate/ftp.go
+++ stencil/migrate/ftp.go
@@ -1,20 +1,45 @@
 package migrate
 import (
-	// "database/sql"
-	// "encoding/json"
-	// "errors"
-	// "fmt"
-	// "github.com/google/uuid"
-	// "log"
-	// "stencil/config"
-	// "stencil/db"
-	// "stencil/app_display"
-	// "stencil/helper"
-	// "stencil/transaction"
-	// "strings"
+	"os"
+	"fmt"
+	"github.com/jlaffaye/ftp"
+	"log"
+	"stencil/db"
+	"strings"
 )
 
 
-func TransferMedia() {
+func TransferMedia(filePath string) error {
 
+	addr := fmt.Sprintf("%s:%s", db.FTP_SERVER_ADDR, db.FTP_SERVER_PORT)
+	
+	log.Println("Dialing FTP Server ", addr)
+	client, err := ftp.Dial(addr)
+	if err != nil {
+		log.Println("Unable to connect to FTP server: ", err)
+		return err
+	}
+
+	log.Println("Trying to log in...")
+	if err := client.Login(db.FTP_USER, db.FTP_PASSWORD); err != nil {
+		log.Println("FTP Login Authentication Error: ", err)
+		return err
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Println("Can't open the file at: ", filePath, err)
+		return err
+	}
+
+	fpTokens := strings.Split(filePath, "/")
+	fileName := fpTokens[len(fpTokens)-1]
+
+	if err := client.Stor("/"+fileName, file); err != nil {
+		log.Println("File Transfer Failed: ", err)
+		return err
+	}
+
+	log.Println("Done!")
+	return nil
 }
