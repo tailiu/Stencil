4115f495ed09834d31bdde17c244f5c219b16439
Zain <zaintq@gmail.com>
2020-02-27 18:15:36 +0400

    bag tests

--- stencil/migrate/deletion.go
+++ stencil/migrate/deletion.go
@@ -17,7 +17,7 @@ func (self *MigrationWorkerV2) DeletionMigration(node *DependencyNode, threadID 
 	} else {
 
 		if strings.EqualFold(node.Tag.Name, rootTagName) {
-			log.Println(fmt.Sprintf("Current   Node { %s } | ID: %v ", color.FgLightYellow.Render(node.Tag.Name), node.Data[nodeIDAttr]))
+			log.Println(fmt.Sprintf("Current   Node { %s } | ID: %v ", color.FgLightCyan.Render(node.Tag.Name), node.Data[nodeIDAttr]))
 			if err := self.CallMigration(node, threadID); err != nil {
 				return err
 			}
@@ -33,8 +33,8 @@ func (self *MigrationWorkerV2) DeletionMigration(node *DependencyNode, threadID 
 				if adjNodeIDAttr, err := adjNode.Tag.ResolveTagAttr("id"); err != nil {
 					self.Logger.Fatalf("adjNode.Tag.ResolveTagAttr(id): %s", err)
 				} else {
-					log.Println(fmt.Sprintf("Current   Node { %s } | ID: %v ", color.FgLightYellow.Render(node.Tag.Name), node.Data[nodeIDAttr]))
-					log.Println(fmt.Sprintf("Adjacent  Node { %s } | ID: %v ", color.FgLightYellow.Render(adjNode.Tag.Name), adjNode.Data[adjNodeIDAttr]))
+					log.Println(fmt.Sprintf("Current   Node { %s } | ID: %v ", color.FgLightCyan.Render(node.Tag.Name), node.Data[nodeIDAttr]))
+					log.Println(fmt.Sprintf("Adjacent  Node { %s } | ID: %v ", color.FgLightCyan.Render(adjNode.Tag.Name), adjNode.Data[adjNodeIDAttr]))
 					if err := self.DeletionMigration(adjNode, threadID); err != nil {
 						self.Logger.Fatal(fmt.Sprintf("ERROR! NODE { %s } | ID: %v, ADJ NODE : { %s } | ID: %v | err: [ %s ]", node.Tag.Name, node.Data[nodeIDAttr], adjNode.Tag.Name, adjNode.Data[adjNodeIDAttr], err))
 						return err
@@ -43,7 +43,7 @@ func (self *MigrationWorkerV2) DeletionMigration(node *DependencyNode, threadID 
 			}
 		}
 
-		log.Println(fmt.Sprintf("PROCESS Node { %s } ", color.FgLightYellow.Render(node.Tag.Name)))
+		log.Println(fmt.Sprintf("PROCESS Node { %s } ", color.FgLightCyan.Render(node.Tag.Name)))
 
 		if strings.EqualFold(node.Tag.Name, rootTagName) {
 			return self.DeleteRoot(threadID)
