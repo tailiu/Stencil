b13e59f5a73a64104589c538a0f17a396aedd44c
Zain Tariq <zaintq@gmail.com>
2020-03-24 18:55:52 +0400

    integrate inner references with migration and create migration cycle controller

---
+++ stencil/run_migration_cycle.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"stencil/SA1_migrate"
+	"stencil/db"
+)
+
+func getUID(appID string) string {
+	var query string
+	switch appID {
+	case "2":
+		{
+			query = "SELECT to_id as id FROM identity_table it where to_app = 2 and to_member = 56 "
+		}
+	case "4":
+		{
+			query = "SELECT to_id as id FROM identity_table it where to_app = 4 and to_member = 155 "
+		}
+	case "3":
+		{
+			query = "SELECT to_id as id FROM identity_table it where to_app = 3 and to_member = 119 "
+		}
+	default:
+		{
+			log.Fatal("Unknown app ", appID)
+		}
+	}
+	dbConn := db.GetDBConn("stencil", false)
+	defer dbConn.Close()
+
+	if result, err := db.DataCall1(dbConn, query); err == nil {
+		return fmt.Sprint(result["id"])
+	} else {
+		fmt.Println("query | ", query)
+		log.Fatal(err)
+	}
+	log.Fatal("Why here?!")
+	return ""
+}
+
+func main() {
+
+	displayInFirstPhase, markAsDelete := false, false
+
+	threads := flag.Int("threads", 1, "")
+	mtype := flag.String("mtype", "d", "")
+	uidInput := flag.String("uid", "", "")
+
+	display := flag.Bool("display", false, "")
+	blade := flag.Bool("blade", false, "")
+	bags := flag.Bool("bags", false, "")
+
+	flag.Parse()
+
+	apps := [][]string{{"diaspora", "1"}, {"mastodon", "2"}, {"gnusocial", "4"}, {"twitter", "3"}}
+
+	totalApps := len(apps)
+
+	for i := 0; i < totalApps; i++ {
+		uid := *uidInput
+		if i != 0 {
+			uid = getUID(apps[i][1])
+		}
+
+		srcAppID, srcAppName := apps[i][0], apps[i][1]
+		var dstAppID, dstAppName string
+
+		if i == totalApps-1 {
+			dstAppID, dstAppName = apps[0][0], apps[0][1]
+		} else {
+			dstAppID, dstAppName = apps[i+1][0], apps[i+1][1]
+		}
+
+		SA1_migrate.Controller(uid, srcAppID, srcAppName, dstAppID, dstAppName, *mtype, *threads, *display, displayInFirstPhase, markAsDelete, *blade, *bags)
+	}
+}
