c78b14f8e676d7786b4bed20fa8dc97d7cfb2a20
Zain Tariq <zaintq@gmail.com>
2020-04-12 13:37:13 +0400

    migration handler v2

--- stencil/migration_handler.v2.go
+++ stencil/migration_handler.v2.go
@@ -14,7 +14,6 @@ import (
 func main() {
 
 	var wg sync.WaitGroup
-	displayInFirstPhase, markAsDelete := false, false
 
 	srcApp := flag.String("srcApp", "diaspora", "")
 	srcAppID := flag.String("srcAppID", "1", "")
@@ -26,86 +25,19 @@ func main() {
 	mtype := flag.String("mtype", "d", "")
 	uid := flag.String("uid", "", "")
 
-	display := flag.Bool("display", false, "")
 	blade := flag.Bool("blade", false, "")
 	bags := flag.Bool("bags", false, "")
 	ftp := flag.Bool("ftp", false, "")
+	display := flag.Bool("display", false, "")
+	displayInFirstPhase := flag.Bool("firstphase", false, "")
+	markAsDelete := flag.Bool("dmad", false, "")
 
 	flag.Parse()
 
 	wg.Add(1)
 
 	go apis.StartMigration(*uid, *srcApp, *srcAppID, *dstApp, *dstAppID, *mtype, *blade, *bags, *ftp)
-	go SA1_display.StartDisplay(*uid, *srcAppID, *dstAppID, *mtype, *threads, &wg, *display, displayInFirstPhase, markAsDelete, *blade)
+	go SA1_display.StartDisplay(*uid, *srcAppID, *dstAppID, *mtype, *threads, &wg, *display, *displayInFirstPhase, *markAsDelete, *blade)
 
 	wg.Wait()
 }
-
-// func main2() {
-// 	// evalConfig := evaluation.InitializeEvalConfig()
-// 	if logTxn, err := transaction.BeginTransaction(); err == nil {
-// 		srcApp, srcAppID := os.Args[4], os.Args[5]
-// 		dstApp, dstAppID := os.Args[6], os.Args[7]
-// 		dflag := "f"
-// 		if len(os.Args) > 8 {
-// 			dflag = os.Args[8]
-// 		}
-// 		threads, err := strconv.Atoi(os.Args[1])
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		uid := os.Args[2]
-
-// 		var mtype string
-
-// 		switch os.Args[3] {
-// 		case "d":
-// 			{
-// 				mtype = migrate.DELETION
-// 			}
-// 		case "i":
-// 			{
-// 				mtype = migrate.INDEPENDENT
-// 			}
-// 		case "c":
-// 			{
-// 				mtype = migrate.CONSISTENT
-// 			}
-// 		}
-
-// 		if len(mtype) <= 0 {
-// 			log.Fatal("can't read migration type")
-// 		}
-
-// 		// mWorker := migrate.CreateLMigrationWorker(uid, srcApp, srcAppID, dstApp, dstAppID, logTxn, mtype)
-// 		mappings := config.GetSchemaMappingsFor(srcApp, dstApp)
-// 		if mappings == nil {
-// 			log.Fatal(fmt.Sprintf("Can't find mappings from [%s] to [%s].", srcApp, dstApp))
-// 		}
-// 		// var wg sync.WaitGroup
-// 		if dflag == "t" {
-// 			// for threadID := 0; threadID < threads; threadID++ {
-// 			// 	wg.Add(1)
-// 			// 	go func() {
-// 			// 		defer wg.Done()
-// 			// 		app_display_algorithm.DisplayThread(dstApp, logTxn.Txn_id)
-// 			// 	}()
-// 			// }
-// 		}
-// 		if mthread.ThreadControllerV2(uid, srcApp, srcAppID, dstApp, dstAppID, logTxn, mtype, mappings, threads) {
-// 			transaction.LogOutcome(logTxn, "COMMIT")
-// 			// wg.Wait()
-// 			db.FinishMigration(logTxn.DBconn, logTxn.Txn_id, 0)
-// 		} else {
-// 			transaction.LogOutcome(logTxn, "ABORT")
-// 		}
-// 		// evaluation.AnomaliesDanglingData(fmt.Sprint(logTxn.Txn_id), evalConfig)
-// 		// evaluation.MigrationRate(fmt.Sprint(logTxn.Txn_id), evalConfig)
-// 		// evaluation.SystemLevelDanglingData(fmt.Sprint(logTxn.Txn_id), evalConfig)
-// 		// evaluation.GetSize(fmt.Sprint(logTxn.Txn_id), evalConfig)
-// 		// evaluation.GetTime(fmt.Sprint(logTxn.Txn_id), evalConfig)
-// 	} else {
-// 		log.Fatal("Can't begin migration transaction", err)
-// 	}
-// 	// migrate.RollbackMigration(1503622861)
-// }
